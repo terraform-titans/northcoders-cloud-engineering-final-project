9896c0d6e8ca5e7df5c3f4ed2016c0d16fe53f74
Joshua Stride <joshcstride@googlemail.com>
2023-06-15 14:32:07 +0100

    Add config for remote backend: dynamoDB table

--- backend.tf
+++ backend.tf
@@ -31,3 +31,16 @@ resource "aws_s3_bucket_versioning" "remote-backend" {
     status = "Enabled"
   }
 }
+
+# Provsioning DynamoDB table for remote state backend
+
+resource "aws_dynamodb_table" "remote-backend" {
+  name         = "remote-backend"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
