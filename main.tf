09baa703ab33ae0c152f7a49c82ddc78b8408c94
Joshua Stride <joshcstride@googlemail.com>
2023-06-15 15:35:16 +0100

    Remove unneeded variable declarations

--- main.tf
+++ main.tf
@@ -3,7 +3,4 @@ module "network" {
 
   aws_region           = var.aws_region
   vpc_name             = var.vpc_name
-  launch_template_name = var.launch_template_name
-  security_group_name  = var.security_group_name
-  load_balancer_name   = var.load_balancer_name
 }
